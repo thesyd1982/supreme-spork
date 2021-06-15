f8a9a47809a3fe89c9154bab7e3b46e92f11ed90
Antoine <antoine.chauvin4444@gmail.com>
2021-06-15 02:20:12 +0200

    add todo

--- todo.py
+++ todo.py
@@ -2,4 +2,6 @@
 Ecrire un paquet de tools
 Ecrire un outil XSS
 Ecrire un README avec https://github.com/RichardLitt/standard-readme/blob/master/README.md
+Completer touts les script avec PayloadAllTheThing pour ajouter des payloads a chaque exploit
+Rassembler toutes les LFI dans une classe / pareil pour les RFI
 """
